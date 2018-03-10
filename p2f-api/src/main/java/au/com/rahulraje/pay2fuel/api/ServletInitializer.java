2aebca904902b0fbfaf91f66206fb1e30cdf0b57
rahul <rahul.raje@gmail.com>
2018-03-11 01:05:24 +1100

    Initial commit for p2f-api

---
+++ p2f-api/src/main/java/au/com/rahulraje/pay2fuel/api/ServletInitializer.java
@@ -0,0 +1,13 @@
+package au.com.rahulraje.pay2fuel.api;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(P2fApiApplication.class);
+	}
+
+}
---
+++ p2f-api/src/test/java/au/com/rahulraje/pay2fuel/api/P2fApiApplicationTests.java
@@ -0,0 +1,16 @@
+package au.com.rahulraje.pay2fuel.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class P2fApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
