93649246797d29928c1dfe92658618950c2f763d
rahul <rahul.raje@gmail.com>
2018-05-23 23:47:03 +1000

    Working OAuth draft

--- p2f-web/src/main/java/au/com/rahulraje/pay2fuel/web/P2fWebApplication.java
+++ p2f-web/src/main/java/au/com/rahulraje/pay2fuel/web/P2fWebApplication.java
@@ -1,9 +1,17 @@
 package au.com.rahulraje.pay2fuel.web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@Configuration
+@EnableOAuth2Sso
+@RestController
 public class P2fWebApplication {
 
 	public static void main(String[] args) {
