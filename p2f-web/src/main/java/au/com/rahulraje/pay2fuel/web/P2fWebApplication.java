145ea98a6993874f351faa3186f3ec37284ca546
rahul <rahul.raje@gmail.com>
2018-06-22 23:13:34 +1000

    Working on Tomcat

--- p2f-web/src/main/java/au/com/rahulraje/pay2fuel/web/P2fWebApplication.java
+++ p2f-web/src/main/java/au/com/rahulraje/pay2fuel/web/P2fWebApplication.java
@@ -4,16 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@ComponentScan(basePackages="au.com.rahulraje.pay2fuel.web")
 @EnableAutoConfiguration
 @Configuration
 @EnableOAuth2Sso
 @RestController
-public class P2fWebApplication {
+public class P2fWebApplication extends SpringBootServletInitializer {
 
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(P2fWebApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(P2fWebApplication.class, args);
 	}
