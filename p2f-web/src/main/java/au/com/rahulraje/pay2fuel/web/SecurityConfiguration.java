ebd773128d68905bfdcf9beaad45dfd865992621
rahul <rahul.raje@gmail.com>
2018-07-14 14:25:08 +1000

    Fixed certificate import step

--- p2f-web/src/main/java/au/com/rahulraje/pay2fuel/web/SecurityConfiguration.java
+++ p2f-web/src/main/java/au/com/rahulraje/pay2fuel/web/SecurityConfiguration.java
@@ -32,6 +32,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated()
 			.and().logout().logoutSuccessUrl("/").permitAll();
 		// @formatter:on
+		http.requiresChannel().anyRequest().requiresSecure();
 	}
 
 }
